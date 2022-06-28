7e52805151417a7e5eebf0296a7eaa0d74d79035
codingwithtashi <khakontas33@gmail.com>
2022-06-28 21:16:51 +0530

    reward add added

--- lib/util/constant.dart
+++ lib/util/constant.dart
@@ -41,6 +41,8 @@ class AppConstant {
       'ca-app-pub-8284901143739274/6651100571';
   static const String BANNER_AD_PRACTICE_MENU_UNIT_ID =
       'ca-app-pub-8284901143739274/9771506718';
+  static const String SPELLING_BEE_REWARD_AD_UNIT_ID =
+      'ca-app-pub-8284901143739274/9784292343';
   static const String TEST_UNIT_ID = 'ca-app-pub-3940256099942544/6300978111';
   static const String APP_ID = 'ca-app-pub-8284901143739274~1421990473';
   static const String SHARE_URL =
